7b1b7eaff6786a103880d63276853a7c929da06b
Daniel Loibl <daniel.loibl@gmail.com>
2024-12-02 20:44:20 +0100

    solve day2 part 2

--- Day2/run.js
+++ Day2/run.js
@@ -12,25 +12,45 @@ function isSafe(report) {
     for (let j = 0; j < report.length - 1; j++) {
         const level = report[j];
         const nextLevel = report[j + 1];
-        if (Math.abs(nextLevel - level) > 3 || level === nextLevel) {
-            return false;
-        }
-        if( Math.sign(nextLevel - level) !== sign){
+        if (Math.abs(nextLevel - level) > 3 || level === nextLevel || Math.sign(nextLevel - level) !== sign) {
             return false
         }
     }
     return true;
 }
 
-function solve(reports = matrix) {
+function removeIndex(array, index) {
+    return array.filter((_, i) => i !== index)
+}
+
+function solvePart1(reports = matrix) {
     let numberOfValidReports = 0;
     for (let report of reports) {
         if (isSafe(report)) {
-            console.log(report)
             numberOfValidReports++;
         }
     }
     console.log("the answer is: ", numberOfValidReports)
 }
 
-solve()
+function safeWithDampener(report = []) {
+    for (let i = 0; i < report.length; i++) {
+        if (isSafe(removeIndex(report, i))) {
+            return true
+        }
+    }
+    return false
+}
+
+function solvePart2(reports = matrix) {
+    let numberOfValidReports = 0;
+    for (let report of reports) {
+        if (isSafe(report) || safeWithDampener(report)) {
+            numberOfValidReports++;
+        }
+    }
+    console.log("the answer to part 2 is: ", numberOfValidReports)
+}
+
+solvePart1()
+solvePart2()
