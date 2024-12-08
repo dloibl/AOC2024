7acde261b1cace0bda69caa26713913d8762db4e
Daniel Loibl <daniel.loibl@gmail.com>
2024-12-08 14:58:36 +0100

    solve day 8 par2

--- Day8/run.js
+++ Day8/run.js
@@ -1,7 +1,6 @@
 const map = require('fs').readFileSync("input", "utf-8").trim().split("\n");
 
-
-const antennas = new Map()
+const input = new Map()
 for (let x = 0; x < map.length; x++) {
     for (let y = 0; y < map[x].length; y++) {
         const point = map[x][y]
@@ -9,11 +8,11 @@ for (let x = 0; x < map.length; x++) {
             console.log([x,y])
         }
         if (point !== "." && point !== "#") {
-            let entry = antennas.get(point);
+            let entry = input.get(point);
             if (entry) {
                 entry.push([x, y]);
             } else {
-                antennas.set(point, [[x, y]]);
+                input.set(point, [[x, y]]);
             }
         }
     }
@@ -23,24 +22,34 @@ const plus = (a, b) => [a[0] + b[0], a[1] + b[1]]
 const minus = (a, b) => [a[0] - b[0], a[1] - b[1]]
 const exists = ([x,y]) => (map[x] || [])[y] != null
 
-const antinodes = new Set()
-for (let [key, values] of antennas) {
-    for (let i = 0; i < values.length; i++) {
-        for (let j = i + 1; j < values.length; j++) {
-            const dx = values[j][0] - values[i][0];
-            const dy = values[j][1] - values[i][1];
-            const p1 = plus(values[j], [dx,dy])
-            const p2 = minus(values[i], [dx,dy])
-            if (exists(p1)) {
-                antinodes.add(p1.join(","))
-            }
-            if (exists(p2)) {
-                antinodes.add(p2.join(","))
+function findAllAntiNodes(antennas = input) {
+    const antinodes = new Set()
+    function findAntiNode(p, v, fun) {
+        antinodes.add(p.join(","))
+        let hasNext = true
+        let q = [...p];
+        while (hasNext) {
+            q = fun(q, v)
+            hasNext = exists(q)
+            if (hasNext) {
+                antinodes.add(q.join(","));
             }
         }
     }
+
+    for (let [_, values] of antennas) {
+        for (let i = 0; i < values.length; i++) {
+            for (let j = i + 1; j < values.length; j++) {
+                const dx = values[j][0] - values[i][0];
+                const dy = values[j][1] - values[i][1];
+                findAntiNode(values[j], [dx, dy], plus)
+                findAntiNode(values[i], [dx, dy], minus)
+            }
+        }
+    }
+
+    return antinodes.size
 }
 
-
-console.log(antinodes.size)
+console.log("the answer is", findAllAntiNodes())
 
